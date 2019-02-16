ac447657b42b224351b20ab3570f15fe3dce12ae
Robby010 <robby0@gmail.com>
2019-02-16 14:53:10 +0200

    sub change

--- SixthGit/src/Github.java
+++ SixthGit/src/Github.java
@@ -4,8 +4,8 @@ public class Github {
 	public static void main(String[] args) {
 System.out.println(5+5+5*5/5);
 System.out.println(6);
-int now = 15;
-System.out.println(now);
+int now = 15; int latter = 10;
+System.out.println(now + latter);
 	}
 
 }
